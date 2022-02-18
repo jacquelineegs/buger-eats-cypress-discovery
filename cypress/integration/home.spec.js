0f7a53e6d8bc8ccc8db308c37de06e217389eeb0
Jacqueline Silva <jacquelinesilva@vipcommerce.com.br>
2022-02-18 16:27:56 -0300

    alteracoes home

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,7 @@
 describe('home page', () => {
-    it('app deve estar online', () => {
+    it('app deve estar online', ()=> {
         cy.visit('/')
-        cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
-    });
+        cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
+    })  
     
-});
+})
