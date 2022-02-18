aeeee8fb8071902179c357d0c9c838fb844e50e6
Jacqueline Silva <jacquelinesilva@vipcommerce.com.br>
2022-02-18 16:38:00 -0300

    alteracao home

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,6 @@
 describe('home page', () => {
     it('app deve estar online', ()=> {
-        cy.visit('/')
+        cy.visit('https://buger-eats-qa.vercel.app')
         cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })  
     
